4441768af7f318c8954c5e2f34881db18ccfbc1a
Matthieu Oger <moger@pixelnest.io>
2014-04-29 01:51:20 +0200

    Hamburger interaction: ok.

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -1,3 +1,18 @@
 $(function () {
+  var toggle = false
 
+  slide = function () {
+    var value = toggle ? -300 : 0
+    toggle = !toggle
+
+    $('#page-nav').animate({'margin-left': value}, 150, 'linear')
+  }
+
+  $('#hamburger').click(function () {
+    slide()
+  })
+
+  $('#page-nav').delegate('a', 'click', function () {
+    slide()
+  })
 })
