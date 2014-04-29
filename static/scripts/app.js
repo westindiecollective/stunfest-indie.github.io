e91c5063e183740b7b39e4dd0c7a8b25d75bc666
Matthieu Oger <moger@pixelnest.io>
2014-04-29 10:37:43 +0200

    Script: fix a bug with the hamburger menu.
    * When the hamburger was deactivated and the window size was resized,
    the menu was still hidden. Now, we observe the window size and reset
    the margin-left of the menu when the size of the window becomes larger
    than 500px (the small-screen threshold).
    
    * We also change the toggle to prevent a double-click the next time the
    hamburger menu is activated AFTER a resize.

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -1,6 +1,7 @@
 $(function () {
   var toggle = false
 
+  // Slide function.
   slide = function () {
     var value = toggle ? -300 : 0
     toggle = !toggle
@@ -8,11 +9,25 @@ $(function () {
     $('#page-nav').animate({'margin-left': value}, 150, 'linear')
   }
 
+  // Add slide on click.
   $('#hamburger').click(function () {
     slide()
   })
 
+  // Slide on click on a list's item.
   $('#page-nav').delegate('a', 'click', function () {
     slide()
+  })
+
+  // Window handler.
+  var w = $(window)
+  w.resize(function () {
+    if (w.width() > 500) {
+      // Remove the margin modification when the size is normal.
+      $('#page-nav').css('margin-left', '')
+
+      // Reset toggle if activated.
+      toggle && (toggle = false)
+    }
   })
 })
